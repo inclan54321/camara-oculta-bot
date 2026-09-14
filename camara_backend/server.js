305098d08c14e881d8827afec7341d52f389a6a3
Inclan54321 <tucorreo@dominio.com>
2026-09-14 16:58:59 -0600

    mover bot a camara_backend

--- camara_backend/server.js
+++ camara_backend/server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 require('dotenv').config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // =====================================================
 // MIDDLEWARES
